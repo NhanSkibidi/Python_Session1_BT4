ca9c183ff188bade8c01b9507d04b2b0a94991b7
NhanSkibidi <nhanluong0808@gmail.com>
2026-05-18 23:01:52 +0700

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,8 @@
+id = input("nhap ma benh nhan: ")
+temperature = float(input("nhap nhiet do: "))
+heartbeat_s = input("Nhap nhip tim: ")
+heartbeat = int(heartbeat_s)
+
+print("ma benh nhan ",id)
+print("nhiet do",temperature)
+print("nhiptim",heartbeat)
